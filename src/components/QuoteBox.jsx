9941735e7b33e64a2c2c616edb9bb9b9d67cfd93
Justin Swiger <justin.swiger@me.com>
2019-12-03 12:38:29 -0500

    initial commit

---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const QuoteBox = () => {
+  return <div>Developer Quotes</div>
+}
+
+export default QuoteBox
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import QuoteBox from '../components/QuoteBox'
 
 const HomePage = () => {
-  return <HelloWorld />
+  return <QuoteBox />
 }
 
 export default HomePage
