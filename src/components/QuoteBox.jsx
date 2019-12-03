6fcf95e67664c3ddba7e73b555a178720ac50736
Justin Swiger <justin.swiger@me.com>
2019-12-03 12:41:16 -0500

    wireframe complete

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const QuoteBox = () => {
-  return <div>Developer Quotes</div>
+  return (
+    <>
+      <section>
+        <h1>Developer Quotes</h1>
+        <img src="" alt="" />
+        <p>Quote</p>
+        <p>- Name</p>
+      </section>
+    </>
+  )
 }
 
 export default QuoteBox
